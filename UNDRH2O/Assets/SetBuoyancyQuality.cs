f52157c2dd493725424f4d8ae28b09d1fe39eab1
DESKTOP-U2RKT7R\Daggett <andrewdecker3526@gmail>
2017-01-04 21:24:51 -0500

    Adding in small amounts of assets at a time

---
+++ UNDRH2O/Assets/SetBuoyancyQuality.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using LostPolygon.DynamicWaterSystem;
+
+public class SetBuoyancyQuality : MonoBehaviour {
+
+	BuoyancyForce[] bf;
+
+	// Use this for initialization
+	void Start ()
+	{
+		bf = FindObjectsOfType<BuoyancyForce> ();
+		foreach (BuoyancyForce b in bf) 
+		{
+			b.Resolution = 1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ UNDRH2O/Assets/ToolRespawn.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolRespawn : MonoBehaviour {
+
+    public GameObject wrenchPrefab, hammerPrefab, tapePrefab, corkPrefab;
+	public Transform wrenchPrefabStartTrans, hammerPrefabStartTrans, tapePrefabStartTrans, corkPrefabStartTrans;
+
+    Transform[] children;
+
+    void Start()
+    {
+        children[0] = (Transform)Instantiate(wrenchPrefab, wrenchPrefabStartTrans.position, wrenchPrefabStartTrans.rotation, transform);
+        children[1] = (Transform)Instantiate(hammerPrefab, hammerPrefabStartTrans.position, hammerPrefabStartTrans.rotation, transform);
+        children[2] = (Transform)Instantiate(tapePrefab, tapePrefabStartTrans.position, tapePrefabStartTrans.rotation, transform);
+        children[3] = (Transform)Instantiate(corkPrefab, corkPrefabStartTrans.position, corkPrefabStartTrans.rotation, transform);
+
+        /*for(int i = 0; i < children.Length; i++)
+        {
+            children[i].GetComponent<>
+        }
+        */
+        
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        if(children[0].parent != transform)
+            children[0] = (Transform)Instantiate(wrenchPrefab, wrenchPrefabStartTrans.position, wrenchPrefabStartTrans.rotation, transform);
+        if (children[1].parent != transform)
+            children[1] = (Transform)Instantiate(hammerPrefab, hammerPrefabStartTrans.position, hammerPrefabStartTrans.rotation, transform);
+        if (children[2].parent != transform)
+            children[2] = (Transform)Instantiate(tapePrefab, tapePrefabStartTrans.position, tapePrefabStartTrans.rotation, transform);
+        if (children[3].parent != transform)
+            children[3] = (Transform)Instantiate(corkPrefab, corkPrefabStartTrans.position, corkPrefabStartTrans.rotation, transform);
+    }
+}
