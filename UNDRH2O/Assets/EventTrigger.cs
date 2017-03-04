3988b88ec85e00e864b6c8f8c7beb0fbe6f14153
James Gartland <jamesngartland@gmail.com>
2017-03-03 20:36:51 -0500

    Added simple Story Manager and some test objects  in a new scene

---
+++ UNDRH2O/Assets/EventTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventTrigger : MonoBehaviour {
+
+	public UnityEvent triggerEvent;
+	//checks for string to avoid too many tags
+	public string requiredName;
+
+	void OnTriggerEnter (Collider hit) 
+	{
+		if (requiredName != null)
+		{
+			if (hit.gameObject.name != requiredName)
+				return;
+		}
+
+		triggerEvent.Invoke ();
+	}
+}
---
+++ UNDRH2O/Assets/StoryManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StoryManager : MonoBehaviour {
+
+	public UnityEvent[] storyEvents;
+	public bool startWithScene;
+
+	public void Start()
+	{
+		if (startWithScene)
+			StartStoryEvent (0);
+	}
+
+	public void StartStoryEvent(int num)
+	{
+		storyEvents [num].Invoke ();
+	}
+}
