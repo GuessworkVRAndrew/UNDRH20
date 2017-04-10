7361cc70eb5e3f866edbd0f698ee3983111abf76
James Gartland <jamesngartland@gmail.com>
2017-04-10 19:13:08 -0400

    Added time triggering to StoryEvent Manager with variable randomness

--- UNDRH2O/Assets/StoryManager.cs
+++ UNDRH2O/Assets/StoryManager.cs
@@ -2,20 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEditor;
 
 public class StoryManager : MonoBehaviour {
 
-	public UnityEvent[] storyEvents;
+	[System.Serializable]
+	public class StoryEvent
+	{
+		[Range(0,10)]
+		public float timeDelay;
+		public float variance;
+		public UnityEvent unityEvent;
+	}
 	public bool startWithScene;
-
+	public StoryEvent[] storyEvents;
 	public void Start()
 	{
 		if (startWithScene)
 			StartStoryEvent (0);
 	}
-
+		
 	public void StartStoryEvent(int num)
 	{
-		storyEvents [num].Invoke ();
+		StartCoroutine (Wait(Random.Range(storyEvents [num].timeDelay - storyEvents [num].variance, storyEvents [num].timeDelay + storyEvents [num].variance)));
+		storyEvents [num].unityEvent.Invoke ();
+	}
+
+	IEnumerator Wait(float num)
+	{
+		yield return new WaitForSeconds (num);
 	}
 }
